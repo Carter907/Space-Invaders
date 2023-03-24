053a4bef1335b80ba829f7d524e4c472443722e6
Carter907 <speers.carter@Gmail.com>
2023-03-24 18:02:34 -0400

    added controls

--- src/main/kotlin/kar/creata/spaceinvaders/controllers/GameController.kt
+++ src/main/kotlin/kar/creata/spaceinvaders/controllers/GameController.kt
@@ -1,11 +1,4 @@
 package kar.creata.spaceinvaders.controllers
 
-import javafx.fxml.Initializable
-import java.net.URL
-import java.util.*
-
-class Game: Initializable{
-    override fun initialize(location: URL?, resources: ResourceBundle?) {
-
-    }
+class Game {
 }
